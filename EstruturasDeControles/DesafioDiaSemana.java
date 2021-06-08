2a4d706b5c7d1a2c68db8baf1b2d3c2ff83b7bbe
IsaqueGabriel <isaquegabriel2002@outlook.com>
2021-06-08 17:48:51 -0300

    Desafio If-Else-If

---
+++ EstruturasDeControles/DesafioDiaSemana.java
@@ -0,0 +1,37 @@
+package EstruturasDeControles;
+
+import java.util.Scanner;
+
+public class DesafioDiaSemana {
+	public static void main(String[] args) {
+		Scanner diaSemana = new Scanner(System.in);
+		
+		System.out.println("Que dia é hoje?");
+		String dia = diaSemana.next();
+		
+		if(dia.equals("Domingo")) {
+			System.out.println(1);
+		}else if(dia.equals("Segunda")) {
+			System.out.println(2);
+		}
+		else if(dia.equals("Terca")) {
+			System.out.println(3);
+		}
+		else if(dia.equals("Quarta")) {
+			System.out.println(4);
+		}
+		else if(dia.equals("Quinta")) {
+			System.out.println(5);
+		}
+		else if(dia.equals("Sexta")) {
+			System.out.println(6);
+		}
+		else if(dia.equals("Sabado")) {
+			System.out.println(7);
+		}else {
+			System.out.println("Erro");
+		}
+		
+		diaSemana.close();
+	}
+}
